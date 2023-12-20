38c8a8a8f62cb0c9a8c8d13f7c111254f5c2e83e
Md. Farhad Islam Shakil <shakilgsc@gmail.com>
2023-12-21 04:49:47 +0600

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'user_name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
